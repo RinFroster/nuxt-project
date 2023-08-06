5d19810bf03d173c7d224f4412b1465dd6fe37ff
TruongViHao <masterofblade69@gmail.com>
2023-08-06 22:30:24 +0700

    Set up Project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <section>
+      <div class="container">Hi</div>
+    </section>
+  </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
