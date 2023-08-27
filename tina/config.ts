693d4abaee991a4af6e996060e6147ce967b0767
TruongViHao <masterofblade69@gmail.com>
2023-08-27 13:07:46 +0700

    Add blog via Tina Admin

--- tina/config.ts
+++ tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "715ea317-7aac-4ffc-98c4-d3061e97e2ba", // Get this from tina.io
-  token: "3a19e5467959f00de2014580e69953285bad3ec5", // Get this from tina.io
+  clientId: process.env.TINA_CLIENT_ID || "", // Get this from tina.io
+  token: process.env.TINA_TOKEN || "", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
